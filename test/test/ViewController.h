cd8bc12545dddb58d2070ef818f23cc0c1c264e8
lanou3g <lanou3g@lanou3gdeMac-mini-8.local>
2015-10-06 14:57:11 +0800

    AddObject

---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright (c) 2015年 蓝欧科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ test/test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  test
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright (c) 2015年 蓝欧科技. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
